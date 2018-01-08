04c377b8453c7012b292eacb810e56d50216392d
Daniel Teobaldo <contato@danielteobaldo.com.br>
2018-01-08 01:54:09 -0200

    Ïnclusao script para tratamentos dados

---
+++ tratarDados.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Faz o tratamento dos dados de financiamento para campanha presidencial dos EUA de 2016.
+Os tratamentos realizados são os seguintes:
+- Verifica o valor da contribuição
+- Separa a data nos componentes dia, mês e ano
+- Cria coluna com o partido dos candidatos
+- Cria coluna com o nome dos comites
+"""
+import csv
+import zipcode
+import pprint
+
+ESTADO_EUA = "TX"
+
+NOME_ARQUIVO = "P00000001-"+ESTADO_EUA+".csv"
+NOME_ARQUIVO_TRATADO = "P00000001-"+ESTADO_EUA+"_tratado.csv"
+
+def recuperar_dados_cidade(zip_number):
+    """
+    Usa um biblioteca de zipcode dos EUA pra equalizar os nomes de cidades
+    da base de dados e obter longitude e latidude.
+    """
+    if zip_number is not None or zip_number != "":
+        zip_number = zip_number[0:5]
+        try:
+            zip = zipcode.isequal(zip_number)
+            if zip is not None:
+                return zip.city, zip.lon, zip.lat
+            else:
+                return "", "", ""
+        except ValueError:
+            return "", "", ""
+    else:
+        return "", "", ""
+
+def tratar_valor_contribuicao(valor_contribuicao):
+    valor_contribuicao = float(valor_contribuicao)
+    valor_contribuicao = abs(valor_contribuicao)
+    return valor_contribuicao
+
+def tratar_data_contribuicao(data):
+    data_separada = data.split('-')
+    dia = data_separada[0]
+    mes = data_separada[1]
+    ano = data_separada[2]
+    return dia, mes, ano
+
+def adicionar_novos_titulos(header):
+    header.append('city')
+    header.append('lon')
+    header.append('lat')
+    header.append('party')
+    header.append('cmte_nm')
+    header.append('cmte_dsgn')
+    header.append('contb_receipt_dt_day')
+    header.append('contb_receipt_dt_month')
+    header.append('contb_receipt_dt_year')
+
+    return header
+
+def tratar_dados_financiamento(dados, candidatos, comite):
+    """
+    Faz o tratamento dos dados do dataset
+    """
+    dados_tratados = []
+    for data in dados:
+        #Trata valores de contribuições negativas
+        valor = tratar_valor_contribuicao(data['contb_receipt_amt'])
+        data['contb_receipt_amt'] = valor
+#        print(posicao)
+
+        data['cmte_nm'] = comite[data['cmte_id']][1]
+        data['cmte_dsgn'] = comite[data['cmte_id']][8]
+
+        data['party'] = candidatos[data['cand_id']][2]
+
+        dia, mes, ano = tratar_data_contribuicao(data['contb_receipt_dt'])
+        data['contb_receipt_dt_day'] = dia
+        data['contb_receipt_dt_month'] = mes
+        data['contb_receipt_dt_year'] = ano
+
+        #Recupera dados das cidades com base no CEP
+        city, lon, lat = recuperar_dados_cidade(data['contbr_zip'])
+        data['city'] = city
+        data['lon'] = lon
+        data['lat'] = lat
+
+        dados_tratados.append(data)
+
+    return dados_tratados
+
+def carregar_dados_financiamento(nome_arquivo):
+    """
+    Carrega os dados de financiamento
+    """
+    dados = []
+    with open(nome_arquivo, 'r') as f:
+        reader = csv.DictReader(f)
+        titulo = reader.fieldnames
+        for row in reader:
+            del row['']
+            dados.append(row)
+    return titulo, dados
+
+def carregar_dados_candidatos(nome_arquivo):
+    """
+    Carrega os dados dos candidatos
+    """
+    dados = {}
+    with open(nome_arquivo, 'r') as f:
+        filedata = csv.reader(f, delimiter='|')
+        for row in filedata:
+            if row[5] == 'P':
+                dados[row[0]] = row
+
+    return dados
+
+def carregar_dados_comite(nome_arquivo):
+    """
+    Carrega os dados dos comites
+    """
+    dados = {}
+    with open(nome_arquivo, 'r') as f:
+        filedata = csv.reader(f, delimiter='|')
+        for row in filedata:
+            dados[row[0]] = row
+
+    return dados
+
+def grava_dados_financiamento(arquivo_saida, dados_tratados, header):
+    """
+    Grava o arquivo com os dados a serem analisados
+    """
+    with open(arquivo_saida, "w") as saida:
+        writer = csv.DictWriter(saida, delimiter=",", fieldnames = header)
+        writer.writeheader()
+        for row in dados_tratados:
+            writer.writerow(row)
+
+if __name__ == "__main__":
+    header, dados = carregar_dados_financiamento(NOME_ARQUIVO)
+    candidatos = carregar_dados_candidatos("cn.txt")
+    comite = carregar_dados_comite("cm.txt")
+    header = adicionar_novos_titulos(header)
+    dados_tratados = tratar_dados_financiamento(dados, candidatos, comite)
+    pprint.pprint(dados_tratados[0])
+    grava_dados_financiamento(NOME_ARQUIVO_TRATADO, dados_tratados, header)
